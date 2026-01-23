e2a719ac6a2ef58590e9fd10e66b76e98b040e47
aliabbassi <ali.alabbassi@heysadie.ai>
2026-01-23 21:38:05 +0200

    fix: delete duplicates before constraint

--- scripts/add_constraint.py
+++ scripts/add_constraint.py
@@ -13,6 +13,13 @@ async def run():
         user=os.getenv('SADIE_DB_USER'),
         password=os.getenv('SADIE_DB_PASSWORD'),
     )
+    print('Deleting duplicates...')
+    deleted = await conn.execute('''
+        DELETE FROM sadie_gtm.hotels a USING sadie_gtm.hotels b
+        WHERE a.id < b.id AND a.source = b.source
+    ''')
+    print(f'Deleted: {deleted}')
+    print('Adding constraint...')
     await conn.execute('ALTER TABLE sadie_gtm.hotels ADD CONSTRAINT hotels_source_unique UNIQUE (source);')
     await conn.close()
     print('Done')
